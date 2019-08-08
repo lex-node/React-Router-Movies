9a909054c6c2a00d6f9e3fcb56c9882839af5277
triggering-event <gabriel.j.shapiro@gmail.com>
2019-08-07 22:35:25 -0400

    adds routes to MovieList and Movie in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {Route} from "react-router-dom";
-
-/** Wrap your app with the router.
- * Inside your App file add two routes.
- * one route for `/` that loads the `MovieList` component.
- * one route that will take an `id` parameter after`/movies/` (ex: `/movies/2`, `/movies/3` where the id is dynamic). This route should load the `Movie` component.
- */
-
 import SavedList from './Movies/SavedList';
+import MovieList from "./Movies/MovieList";
+import Movie from "./Moves/Movie";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+    const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+    const addToSavedList = movie => {
+        setSavedList([...savedList, movie]);
+    };
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+    return (
+        <div>
+            <SavedList list={savedList}/>
+            <div>
+                <Route exact path="/" component={MovieList}/>
+                <Route path="/movies/{id}" component={Movie}/>
+            </div>
+
+        </div>
+    );
 };
 
 export default App;
