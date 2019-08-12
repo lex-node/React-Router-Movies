c3ed226254663697b3bf132fb46dfbeda673fd12
triggering-event <gabriel.j.shapiro@gmail.com>
2019-08-11 22:51:59 -0400

    turns each movie card on movielist into a link, each card then links to a page corresponding to the specific movie for that card

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,57 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
-  useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
+    const [movie, setMovie] = useState();
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+    useEffect(() => {
+        const id = props.match.params.id;
+        // change ^^^ that line and grab the id from the URL
+        // You will NEED to add a dependency array to this effect hook
 
-  },[]);
-  
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+        axios
+            .get(`http://localhost:5000/api/movies/${id}`)
+            .then(response => {
+                setMovie(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
 
-  if (!movie) {
-    return <div>Loading movie information...</div>;
-  }
+    }, []);
 
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
+    // Uncomment this only when you have moved on to the stretch goals
+    // const saveMovie = () => {
+    //   const addToSavedList = props.addToSavedList;
+    //   addToSavedList(movie)
+    // }
+
+    if (!movie) {
+        return <div>Loading movie information...</div>;
+    }
+    console.log(movie);
+    const {title, director, metascore, stars} = movie;
+    console.log(title);
+    return (
+        <div className="save-wrapper">
+            <div className="movie-card">
+                <h2>{title}</h2>
+                <div className="movie-director">
+                    Director: <em>{director}</em>
+                </div>
+                <div className="movie-metascore">
+                    Metascore: <strong>{metascore}</strong>
+                </div>
+                <h3>Actors</h3>
+
+                {stars.map(star => (
+                    <div key={star} className="movie-star">
+                        {star}
+                    </div>
+                ))}
+            </div>
+            <div className="save-button">Save</div>
         </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+    );
 }
 
 export default Movie;
+
+/*### Part 2:
+
+* Add functionality so the `Home` button on the `SavedList` component navigates back to home.
+* You should now be able to navigate back and forth between the individual movies and the home screen.*/
