5558be44e7c14550dc56a0c641ef47ea4872be87
triggering-event <gabriel.j.shapiro@gmail.com>
2020-05-03 16:25:48 -0400

    completes part 1 of MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
+import {Route, Link, Switch, useParams} from 'react-router-dom'
 import axios from 'axios';
-
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
+    const [savedList, setSavedList] = useState([]);
+    const [movieList, setMovieList] = useState([]);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovieList(response.data);
+    useEffect(() => {
+        const getMovies = () => {
+            axios
+                .get('http://localhost:5000/api/movies')
+                .then(response => {
+                    console.log(response.data);
+                    setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -26,10 +29,13 @@ const App = () => {
   };
 
   return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
+      <div>
+          <SavedList list={savedList}/>]
+          <Switch>
+              <Route exact path='/' component={() => <MovieList movies={movieList}/>}/>
+              <Route path={`/movies/:id`} component={Movie}/>
+          </Switch>
+      </div>
   );
 };
 
